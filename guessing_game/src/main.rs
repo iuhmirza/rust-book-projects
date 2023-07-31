d2ae9d0eaf75a808fd36bf728cf708b5288091e5
Ibtisam Mirza <iuhmirza@gmail.com>
2023-07-31 12:48:11 +0100

    Loop infinitely for user guesses

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,20 +6,23 @@ fn main() {
     println!("I'm going to think of a number between 1 and 100!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Guess? ");
-    let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Guess? ");
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse().expect("Please guess a number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+        let guess: u32 = guess.trim().parse().expect("Please guess a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
