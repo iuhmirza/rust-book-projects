52367ba813edf4d2ea1b7f2ce773b33149307d6a
Ibtisam Mirza <iuhmirza@gmail.com>
2023-07-31 10:47:01 +0100

    Use cargo to create rust project

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/ibtisam/repos/rust-book-projects/hello_cargo/target/debug/hello_cargo: /home/ibtisam/repos/rust-book-projects/hello_cargo/src/main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/ibtisam/repos/rust-book-projects/hello_cargo/target/release/hello_cargo: /home/ibtisam/repos/rust-book-projects/hello_cargo/src/main.rs
